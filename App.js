b2012298b65e3b7f766ef0ef129d7c03040ff982
Serhii-Buha <serhii.buga@gmail.com>
2023-06-16 16:41:25 +0300

    add App, expo-font, react-navigation/native, react-navigation/stack, react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const MainNavigator = createStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto: require('./src/fonts/Roboto-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <MainNavigator.Navigator initialRouteName="Login">
+          {/* <MainNavigator.Screen name="Login" component={LoginScreen} /> */}
+          <MainNavigator.Screen
+            name="Register"
+            component={RegistrationScreen}
+          />
+          
+        </MainNavigator.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
@@ -14,7 +39,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
