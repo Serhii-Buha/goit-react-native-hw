d7805f0a64988b75ddaf2258b2e068b7d5caeb20
Serhii-Buha <serhii.buga@gmail.com>
2023-06-17 09:40:11 +0300

    add RegistrationScreen, useState, styles

--- src/screens/RegistrationScreen.jsx
+++ src/screens/RegistrationScreen.jsx
@@ -35,7 +35,6 @@ export default function RegistrationScreen() {
 
   const onPress = () => {
     console.log('login:', login, 'email:', email, 'password:', password);
-    navigation.navigate('Home');
   };
 
   return (
