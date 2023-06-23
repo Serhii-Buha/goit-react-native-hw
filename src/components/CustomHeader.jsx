702aac1cd49b4cb28f3787df74e168fd9458bb8d
Serhii-Buha <serhii.buga@gmail.com>
2023-06-23 13:48:18 +0300

    add component CustomHeader, useNavigation, expo/vector-icons, Text, View, styles

---
+++ src/components/CustomHeader.jsx
@@ -0,0 +1,51 @@
+import { Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { AntDesign } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { styles } from '../screens/scc';
+
+const CustomHeader = ({ title, options }) => {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <View style={styles.customHeader}>
+        <Text style={styles.customHeaderText}>{title}</Text>
+        {title === 'Posts' ? (
+          <MaterialIcons
+            name="logout"
+            size={24}
+            color="#BDBDBD"
+            style={{
+              marginLeft: 'auto',
+              position: 'absolute',
+              right: 10,
+              top: 10,
+            }}
+            onPress={() => navigation.navigate('Login')}
+          />
+        ) : (
+          <AntDesign
+            name="arrowleft"
+            size={24}
+            color="#BDBDBD"
+            style={{
+              marginLeft: 'auto',
+              position: 'absolute',
+              left: 10,
+              top: 10,
+            }}
+            onPress={() => navigation.navigate('Posts')}
+          />
+        )}
+      </View>
+    </>
+  );
+};
+
+export default CustomHeader;
