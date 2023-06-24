fd9ac5b551b600f68265094ba9ddc72f2d045656
Serhii-Buha <serhii.buga@gmail.com>
2023-06-24 17:33:57 +0300

    add useEffect, expo-location, expo-camera, expo-media-library, @react-navigation/native

--- src/screens/CreatePostsScreen.jsx
+++ src/screens/CreatePostsScreen.jsx
@@ -7,20 +7,71 @@ import {
   Keyboard,
   KeyboardAvoidingView,
   TextInput,
-  Platform,
   TouchableWithoutFeedback,
 } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import * as Location from 'expo-location';
 import { styles } from './scc';
+import { useNavigation } from '@react-navigation/native';
 
-const CreatePostsScreen = () => {
-  const [havePhoto, setHavePhoto] = useState(false);
+export default function CreatePostsScreen() {
+  const [permission, setPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photoUri, setPhotoUri] = useState(null);
   const [name, setName] = useState('');
   const [location, setLocation] = useState('');
+  const [geoLocation, setGeoLocation] = useState(null);
 
-  const haveParam = havePhoto && !!name && !!location;
+  const haveParam = photoUri && !!name && !!location;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+      setPermission(status === 'granted');
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      const coords = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      };
+      setGeoLocation(coords);
+    })();
+  }, []);
+
+  if (permission === null) {
+    return <View />;
+  }
+  if (permission === false) {
+    navigation.navigate('Posts');
+  }
+
+  const onPostPress = () => {
+    //do something
+    onDelPress();
+    navigation.navigate('Posts');
+  };
+  const onDelPress = () => {
+    setPhotoUri(null);
+    setName('');
+    setLocation('');
+    setGeoLocation(null);
+  };
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -34,19 +85,51 @@ const CreatePostsScreen = () => {
           display: 'flex',
         }}
       >
-        <View style={innerStyles.imageContainer}>
-          <Image />
-          <View style={innerStyles.photoIcon}>
-            <MaterialIcons
-              name="photo-camera"
-              size={24}
-              color={!havePhoto ? '#BDBDBD' : 'white'}
-            />
+        <Camera
+          style={innerStyles.imageContainer}
+          type={type}
+          ref={setCameraRef}
+        >
+          <View>
+            <View style={innerStyles.photoIcon}>
+              <MaterialIcons
+                name="photo-camera"
+                size={24}
+                color={!photoUri ? '#BDBDBD' : 'white'}
+                onPress={async () => {
+                  if (cameraRef) {
+                    const { uri } = await cameraRef.takePictureAsync();
+                    setPhotoUri(uri);
+                    await MediaLibrary.createAssetAsync(uri);
+                  }
+                }}
+              />
+            </View>
           </View>
-        </View>
-        <Text style={innerStyles.innerText}>
-          {!havePhoto ? 'Add photo' : 'Edit photo'}
-        </Text>
+        </Camera>
+        <TouchableOpacity
+          style={styles.flipContainer}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        >
+          <Text
+            style={{
+              ...innerStyles.innerText,
+              marginBottom: 10,
+              color: 'black',
+            }}
+          >
+            Flip camera
+          </Text>
+        </TouchableOpacity>
+        {/* <Text style={innerStyles.innerText}>
+          {!photoUri ? "Add photo" : "Edit photo"}
+        </Text> */}
         <KeyboardAvoidingView
           behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
         >
@@ -82,6 +165,7 @@ const CreatePostsScreen = () => {
               ? styles.button
               : { ...styles.button, backgroundColor: '#F6F6F6' }
           }
+          onPress={onPostPress}
         >
           <Text
             style={
@@ -102,12 +186,17 @@ const CreatePostsScreen = () => {
             alignSelf: 'center',
           }}
         >
-          <AntDesign name="delete" size={24} color="#bdbdbd" />
+          <AntDesign
+            name="delete"
+            size={24}
+            color="#bdbdbd"
+            onPress={onDelPress}
+          />
         </View>
       </View>
     </TouchableWithoutFeedback>
   );
-};
+}
 
 const innerStyles = StyleSheet.create({
   imageContainer: {
@@ -129,6 +218,7 @@ const innerStyles = StyleSheet.create({
     height: 60,
     width: 60,
     backgroundColor: 'white',
+    opacity: 0.5,
     borderRadius: 30,
   },
   innerText: {
@@ -150,5 +240,3 @@ const innerStyles = StyleSheet.create({
     borderBottomWidth: 1,
   },
 });
-
-export default CreatePostsScreen;
