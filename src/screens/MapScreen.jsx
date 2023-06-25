1de708e189c087130df02be16238e9037d821194
Serhii-Buha <serhii.buga@gmail.com>
2023-06-25 17:45:31 +0300

    add MapScreen, react-native-maps, MapView, Marker, View, styles

--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -1,7 +1,62 @@
-import { Text } from 'react-native';
+import {
+  Button,
+  TextInput,
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Dimensions,
+} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-const MapScreen = () => {
-  return <Text>MapScreen</Text>;
+const MapScreen = ({ navigation, route, options }) => {
+  const latitude = route.params.geoLocation.latitude;
+  const longitude = route.params.geoLocation.longitude;
+  console.log(latitude, longitude);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        minZoomLevel={15}
+        // onMapReady={() => console.log('Map is ready')}
+        // onRegionChange={() => console.log('Region change')}
+      >
+        <Marker
+          title={route.params.name}
+          coordinate={{ latitude: latitude, longitude: longitude }}
+          description={route.params.location}
+        />
+      </MapView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default MapScreen;
