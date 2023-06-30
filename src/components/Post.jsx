d04139670b806143ecb96321bd8705ab21e843db
Serhii-Buha <serhii.buga@gmail.com>
2023-06-30 12:41:10 +0300

    Post, add useDispatch, useSelector, getEmail, addLike, delPost,  styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,49 @@
-import {
-  Button,
-  TextInput,
-  View,
-  ScrollView,
-  Text,
-  StyleSheet,
-  ImageBackground,
-  Image,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Dimensions,
-} from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import { getEmail } from '../redux/selectors';
+import { addLike, delPost } from '../redux/thunks';
 
 const Post = ({ data }) => {
   const navigation = useNavigation();
-  const { geoLocation, location, name, photoUri } = data;
+  const accEmail = useSelector(getEmail);
+  const dispatch = useDispatch();
+
+  const {
+    geoLocation,
+    location,
+    name,
+    url,
+    email,
+    creationTime,
+    comments,
+    likes,
+  } = data;
+
+  const delPostFunc = () => {
+    dispatch(delPost(creationTime));
+  };
+
+  const addLikeFunc = () => {
+    if (likes?.includes(accEmail)) return;
+    dispatch(addLike({ mail: accEmail, id: creationTime }));
+  };
+
   return (
     <View style={postStyles.container}>
-      <Image style={postStyles.image} source={{ uri: photoUri }} />
+      <Image style={postStyles.image} source={{ uri: url }} />
+      {accEmail === email && (
+        <View style={postStyles.bottomNavigation}>
+          <AntDesign
+            name="delete"
+            size={24}
+            color={'#bdbdbd'}
+            onPress={delPostFunc}
+          />
+        </View>
+      )}
       <Text style={{ ...postStyles.text, marginBottom: 11 }}>{name}</Text>
       <View
         style={{
@@ -32,18 +52,42 @@ const Post = ({ data }) => {
           justifyContent: 'space-between',
         }}
       >
-        <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <View style={postStyles.viewStyle}>
           <FontAwesome
-            name="comment-o"
+            name="comment"
             size={24}
-            color="#BDBDBD"
-            onPress={() => navigation.navigate('Comments')}
+            color={comments?.length ? '#FF6C00' : '#BDBDBD'}
+            onPress={() =>
+              navigation.navigate('Comments', { creationTime, url })
+            }
           />
-          <Text style={{ ...postStyles.text, color: '#BDBDBD', marginLeft: 8 }}>
-            Num
+          <Text
+            style={
+              comments?.length
+                ? { ...postStyles.text, marginLeft: 8 }
+                : { ...postStyles.text, color: '#BDBDBD', marginLeft: 8 }
+            }
+          >
+            {comments?.length}
+          </Text>
+          <AntDesign
+            name="like2"
+            size={24}
+            style={{ marginLeft: 10 }}
+            color={likes?.length ? '#FF6C00' : '#BDBDBD'}
+            onPress={addLikeFunc}
+          />
+          <Text
+            style={
+              likes?.length
+                ? { ...postStyles.text, marginLeft: 8 }
+                : { ...postStyles.text, color: '#BDBDBD', marginLeft: 8 }
+            }
+          >
+            {likes?.length ?? 0}
           </Text>
         </View>
-        <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <View style={postStyles.viewStyle}>
           <AntDesign
             name="enviromento"
             size={24}
@@ -60,14 +104,17 @@ const Post = ({ data }) => {
 };
 
 export const postStyles = StyleSheet.create({
-  container: {},
+  container: {
+    position: 'relative',
+    marginBottom: 15,
+  },
   image: {
     width: '100%',
     height: 240,
     backgroundColor: '#bdbdbd',
     borderRadius: 8,
     marginBottom: 8,
-    marginTop: 32,
+    marginTop: 0,
   },
   text: {
     fontFamily: 'Roboto',
@@ -76,6 +123,25 @@ export const postStyles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 19,
     color: '#212121',
+  },
+  bottomNavigation: {
+    height: 40,
+    width: 70,
+    borderRadius: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F6F6F6',
+    alignSelf: 'center',
+    position: 'absolute',
+    top: 5,
+    left: 5,
+  },
+  viewStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
