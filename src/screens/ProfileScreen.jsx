ef4a7459fceb8ff0dfab00f627f670ecb67621f3
Serhii-Buha <serhii.buga@gmail.com>
2023-06-30 13:15:18 +0300

    ProfileScreen, useSelector, dispatch(logOutUser()), styles

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,66 +1,95 @@
 import {
-  Button,
-  TextInput,
   View,
   ScrollView,
   Text,
   StyleSheet,
   ImageBackground,
   Image,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableWithoutFeedback,
-  Keyboard,
+  Dimensions,
 } from 'react-native';
 import ImageBG from '../PhotoBG.png';
-import { styles } from './scc';
-import { useState } from 'react';
-import { AntDesign } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import Post from '../components/Post';
+import { getFilteredPosts, getUser } from '../redux/selectors';
+import { logOutUser } from '../redux/thunks';
 
 const ProfileScreen = () => {
-  const [photo, setPhoto] = useState(null);
+  const dispatch = useDispatch();
   const navigation = useNavigation();
+  const { login, email, photoUri } = useSelector(getUser);
+  const posts = useSelector(getFilteredPosts(email));
 
   return (
     <ImageBackground source={ImageBG} style={styles.imageBG}>
       <View style={styles.container}>
-        <ImageBackground source={photo} style={styles.image}>
-          {!photo ? (
-            <AntDesign
-              name="pluscircleo"
-              size={24}
-              color="#FF6C00"
-              style={styles.icon}
-            />
-          ) : (
-            <AntDesign
-              name="closecircleo"
-              size={24}
-              color="#BDBDBD"
-              style={styles.icon}
-            />
-          )}
-        </ImageBackground>
+        <Image source={{ uri: photoUri }} style={styles.avatar} />
         <MaterialIcons
           name="logout"
           size={24}
           color="#BDBDBD"
-          style={{
-            marginLeft: 'auto',
-            position: 'absolute',
-            right: 16,
-            // top: 10,
-            paddingVertical: 10,
-            paddingLeft: 16,
+          style={styles.icon}
+          onPress={() => {
+            dispatch(logOutUser());
+            navigation.navigate('Login');
           }}
-          onPress={() => navigation.navigate('Login')}
         />
+        <Text style={styles.text}>{login}</Text>
+        {posts && (
+          <ScrollView style={{ paddingHorizontal: 16 }}>
+            {posts.map(el => (
+              <Post key={el.creationTime} data={el}></Post>
+            ))}
+          </ScrollView>
+        )}
       </View>
     </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  imageBG: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  container: {
+    position: 'relative',
+    marginTop: 120,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingTop: 60,
+    paddingBottom: 30,
+    minHeight: Dimensions.get('window').height - 120,
+    backgroundColor: 'white',
+    opacity: 1,
+  },
+  avatar: {
+    height: 120,
+    width: 120,
+    backgroundColor: '#F6F6F6',
+    position: 'absolute',
+    top: -60,
+    left: Dimensions.get('window').width / 2,
+    borderRadius: 16,
+    transform: [{ translateX: -60 }],
+  },
+  icon: {
+    marginLeft: 'auto',
+    position: 'absolute',
+    right: 10,
+    top: 10,
+  },
+  text: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'center',
+    color: '#212121',
+  },
+});
 
 export default ProfileScreen;
