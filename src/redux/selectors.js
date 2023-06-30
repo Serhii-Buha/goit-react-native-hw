c4114000bb2ae5bf5930eea29b22682d69d103dd
Serhii-Buha <serhii.buga@gmail.com>
2023-06-30 11:41:57 +0300

    add selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,1 +1,15 @@
+export const getUid = state => state.user.uid;
+export const getLogin = state => state.user.isLogin;
+export const getPosts = state => state.posts.posts;
+export const getUser = state => state.user;
 export const getEmail = state => state.user.email;
+export const getPost = id => state => {
+  const posts = state.posts.posts;
+  const [post] = posts.filter(post => post.creationTime === id);
+  return post.comments;
+};
+export const getFilteredPosts = email => state => {
+  const posts = state.posts.posts;
+  const filteredPosts = posts.filter(post => post.email === email);
+  return filteredPosts;
+};
